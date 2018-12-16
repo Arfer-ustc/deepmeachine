291f377bb83c6566f92cf71a3eab75f3dc2168c7
xuefei <1403193782@qq.com>
2018-12-16 13:14:17 +0800

    USTC-机器学习实验一贝叶斯实现糖尿病患者分类 以及酒的分类

---
+++ machinelearning/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/12/15 下午5:46
+# @Author  : xuef
+# @FileName: __init__.py.py
+# @Software: PyCharm
+# @Blog    ：https://blog.csdn.net/weixin_42118777/article
---
+++ machinelearning/pima-indians-diabetes.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/12/15 下午6:37
+# @Author  : xuef
+# @FileName: pima-indians-diabetes.py
+# @Software: PyCharm
+# @Blog    ：https://blog.csdn.net/weixin_42118777/article
+#怀孕次数、口服葡萄糖耐量试验中 2 小时血浆葡萄糖浓度、舒张压、三头肌皮褶厚度、2 小时血清胰岛素、体重指数 kg/m^2、糖尿病家族作用、年龄、糖尿病。如果是，则为 1，否则为 0
+import csv
+import random
+import math
+
+
+def loadCsv(filename):
+    f=open(filename,"r")
+    reader=csv.reader(f)
+    dataset=list(reader)
+    # lines = csv.reader(open(filename, "rb"))
+    # dataset = list(lines)
+    for i in range(len(dataset)):
+        dataset[i] = [float(x) for x in dataset[i]]
+    return dataset
+
+
+def splitDataset(dataset, splitRatio):
+    trainSize = int(len(dataset) * splitRatio)
+    trainSet = []
+    copy = list(dataset)
+    while len(trainSet) < trainSize:
+        index = random.randrange(len(copy))
+        trainSet.append(copy.pop(index))
+    return [trainSet, copy]
+
+#SeparateByClass()函数按类别划分数据，然后计算出每个类的统计数据。
+def separateByClass(dataset):
+    separated = {}
+    for i in range(len(dataset)):
+        vector = dataset[i]
+        if (vector[-1] not in separated):
+            separated[vector[-1]] = []
+        separated[vector[-1]].append(vector)
+    return separated
+
+#计算在每个类中每个属性的均值、每个类中每个属性的标准差。均值是数据的中点或者集中 趋势，
+# 在计算概率时，用来作为高斯分布的中值
+def mean(numbers):
+    return sum(numbers) / float(len(numbers))
+
+
+def stdev(numbers):
+    avg = mean(numbers)
+    variance = sum([pow(x - avg, 2) for x in numbers]) / float(len(numbers) - 1)
+    return math.sqrt(variance)
+
+
+def summarize(dataset):
+    summaries = [(mean(attribute), stdev(attribute)) for attribute in zip(*dataset)]
+    #删除最后一个，因为最后一个是结果，对属性特征的提取无用
+    del summaries[-1]
+    return summaries
+
+
+def summarizeByClass(dataset):
+    separated = separateByClass(dataset)
+    summaries = {}
+    for classValue, instances in separated.items():
+        summaries[classValue] = summarize(instances)
+    return summaries
+
+#给定来自训练数据中已知属性的均值和标准差，可以使用高斯函数来评估一个给定的属性值 的概率
+def calculateProbability(x, mean, stdev):
+    exponent = math.exp(-(math.pow(x - mean, 2) / (2 * math.pow(stdev, 2))))
+    return (1 / (math.sqrt(2 * math.pi) * stdev)) * exponent
+
+#合并一个数据样本中所有属性的概率，得到整个数据样本属于某个类的概率。
+def calculateClassProbabilities(summaries, inputVector):
+    probabilities = {}
+    for classValue, classSummaries in summaries.items():
+        probabilities[classValue] = 1
+        for i in range(len(classSummaries)):
+            mean, stdev = classSummaries[i]
+            x = inputVector[i]
+            probabilities[classValue] *= calculateProbability(x, mean, stdev)
+    return probabilities
+
+
+def predict(summaries, inputVector):
+    probabilities = calculateClassProbabilities(summaries, inputVector)
+    bestLabel, bestProb = None, -1
+    for classValue, probability in probabilities.items():
+        if bestLabel is None or probability > bestProb:
+            bestProb = probability
+            bestLabel = classValue
+    return bestLabel
+
+#通过对测试数据集中每个数据样本的预测，我们可以评估模型精度。
+# getPredictions()函数实 现这个功能，并返回每个测试样本的预测列表
+def getPredictions(summaries, testSet):
+    predictions = []
+    for i in range(len(testSet)):
+        result = predict(summaries, testSet[i])
+        predictions.append(result)
+    return predictions
+
+
+def getAccuracy(testSet, predictions):
+    correct = 0
+    for i in range(len(testSet)):
+        if testSet[i][-1] == predictions[i]:
+            correct += 1
+    return (correct / float(len(testSet))) * 100.0
+
+
+def main():
+    filename = 'pima-indians-diabetes.csv'
+    splitRatio = 0.67
+    dataset = loadCsv(filename)
+    trainingSet, testSet = splitDataset(dataset, splitRatio)
+    # print('Split {0} rows into train={1} and test={2} rows').format(len(dataset), len(trainingSet), len(testSet))
+    # prepare model
+    summaries = summarizeByClass(trainingSet)
+    # test model
+    predictions = getPredictions(summaries, testSet)
+    accuracy = getAccuracy(testSet, predictions)
+    # print('Accuracy: {0}%').format(accuracy)
+    print("Accuracy:%f"%accuracy)
+
+
+main()
