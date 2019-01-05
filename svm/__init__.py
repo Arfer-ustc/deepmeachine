6f3fbfd17ab1454bbe627a0d89bc54126adc940b
xuefei <1403193782@qq.com>
2019-01-05 20:27:14 +0800

    实验报告要求

---
+++ svm/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/1/5 下午12:10
+# @Author  : xuef
+# @FileName: __init__.py.py
+# @Software: PyCharm
+# @Blog    ：https://blog.csdn.net/weixin_42118777/article
